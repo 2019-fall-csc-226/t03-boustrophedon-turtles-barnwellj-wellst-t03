17ccf5b91feb896f17d67350abc42ac39a54b672
barnwellj <barnwellj@berea.edu>
2019-09-11 10:22:14 -0400

    Finished with T03

--- t03_stub.py
+++ t03_stub.py
@@ -1,8 +1,8 @@
 #################################################################################
-# Author:
-# Username:
+# Author: Jacob Barnwell Taran Wells
+# Username: barnwellj wellst
 #
-# Assignment:
+# Assignment: TO3
 # Purpose:
 #################################################################################
 # Acknowledgements:
@@ -11,50 +11,68 @@
 #################################################################################
 
 import turtle
-wn = turtle.Screen()
-jimmy = turtle.Turtle()
-jimmy.color("black")
-jimmy.pensize(22)
 
 
-
-
-
-
-
-def function_1():
+def draw_square(jimmy):
     """Creates the box for the drawing
     """
-jimmy.penup()
-jimmy.setpos(-250,250)
-jimmy.pendown()
+    jimmy.penup()
+    jimmy.setpos(-250,250)
+    jimmy.pendown()
 
-for s in range(4):
-    jimmy.forward(500)
+    for s in range(4):
+        jimmy.forward(594)
+        jimmy.right(90)
+        pass
+        # ...
+
+
+def right_turns(jimmy):
+    """right turns within the box
+    """
+    jimmy.forward(550)
     jimmy.right(90)
-
-
+    jimmy.forward(22)
+    jimmy.right(90)
     pass
     # ...
-
-
-def function_2():
+def left_turns(jimmy):
+    """left turns within the box
     """
-    Docstring for function_2
-    """
+    jimmy.forward(550)
+    jimmy.left(90)
+    jimmy.forward(22)
+    jimmy.left(90)
     pass
-    # ...
-
 
 def main():
     """
     Docstring for main
     """
     # ...
-    function_1()            # Function call to function_1
-    function_2()            # Function call to function_2
+    wn = turtle.Screen()
+    jimmy = turtle.Turtle()
+    jimmy.color("black")  # importing the Turtle and screen, applying attributes
+    jimmy.pensize(22)
+    jimmy.speed(15)
 
+    draw_square(jimmy)            # Function to draw the main square
+    jimmy.penup()
+    jimmy.forward(22)
+    jimmy.right(90)
+    jimmy.forward(22)
+    jimmy.left(90)
+    jimmy.color("blue")
+    jimmy.pendown()
+
+    for i in range(12):
+      right_turns(jimmy)            # Function call to function_2
+      left_turns(jimmy)            # Function call to function_3
+
+    right_turns(jimmy)              #finishes the fill
+    jimmy.forward(550)
+
+    wn.exitonclick()
 
 main()
 
-wn.exitonclick()
